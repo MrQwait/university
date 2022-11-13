8f3c08b8eeabc945d54080ab4b4207b5820b7fab
MrQwait <113439307+MrQwait@users.noreply.github.com>
2022-11-13 13:32:55 +0300

    Update functions_for_PW3.py

--- Tasks/Practical work/PW3Field_of_miracles/functions_for_PW3.py
+++ Tasks/Practical work/PW3Field_of_miracles/functions_for_PW3.py
@@ -8,6 +8,16 @@ import random
 
 def words():
     return random.choice(w)
-#def max_record(a):
-    #with open('record.txt', 'a') as f:
-        #f.write(a)
+
+def max_record(a):
+    rec = open('record.txt')
+    general_record = rec.read()
+    rec.close()
+    if int(general_record)>=a:
+        print(f'Общий рекорд: {general_record}\n')
+    if int(general_record)<a:
+         print(f'Поздравляю! У вас новый рекорд')
+         rec = open('record.txt', mode='w')
+         rec.write(str(a))
+         print(f'Общий рекорд:{a}\n')
+         rec.close()
