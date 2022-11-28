74f21c01539a40a1a2429476060f5f6c7ff62148
MrQwait <113439307+MrQwait@users.noreply.github.com>
2022-11-28 19:58:43 +0300

    Update PW2Birthday.py

--- Tasks/Practical work/PW2Birthday.py
+++ Tasks/Practical work/PW2Birthday.py
@@ -1,24 +1,32 @@
 import random
-def birthay(a):
-    count=0
-    k=[]
-    day=[]
-    month=[]
 
-    for i in range (0, a):
-        k.append(i+1)
-        day.append(random.randint(1,28))
-        month.append(random.randint(1,12))
 
-    print(k,'\n',day,'\n', month, sep='')
+def birthay(count_students: int) -> str:
+    '''
+    Парадокс дней рождения
+    :param count_students: число (количество учащихся)
+    :return: процент совпадения дней рождений
+    '''
+    count: int = 0
+    students: list[int] = []
+    day: list[int] = []
+    month: list[int] = []
 
-    for i in range (0, len(k)-1):
-        for d in range (i+1,len(k)):
-            if day[i]==day[d] and month[i]==month[d]:
-                count=count + 2
-                print(k[i],day[i],month[i])
-                print(k[d], day[d], month[d])
-    print(int((count/a) * 100),'%',sep='')
-a=int(input())
-birthay(a)
+    for i in range(0, count_students):
+        students.append(i + 1)
+        day.append(random.randint(1, 28))
+        month.append(random.randint(1, 12))
 
+    print(students, '\n', day, '\n', month, sep='')
+
+    for i in range(0, len(students) - 1):
+        for d in range(i + 1, len(students)):
+            if day[i] == day[d] and month[i] == month[d]:
+                count = count + 2
+                print(students[i], day[i], month[i])
+                print(students[d], day[d], month[d])
+    return (str(int((count / a) * 100)) + '%')
+
+
+a = int(input())
+print(birthay(a))
