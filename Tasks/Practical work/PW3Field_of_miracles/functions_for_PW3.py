7a04aa5d45c7ff32d436075779325b062a0ba38b
MrQwait <113439307+MrQwait@users.noreply.github.com>
2022-11-28 20:33:42 +0300

    Update functions_for_PW3.py

--- Tasks/Practical work/PW3Field_of_miracles/functions_for_PW3.py
+++ Tasks/Practical work/PW3Field_of_miracles/functions_for_PW3.py
@@ -1,5 +1,5 @@
 text = open('words.txt', encoding='utf-8')
-w=text.readlines()
+w: list[str] = text.readlines()
 for i in range(len(w)):
     if '\n' in w[i]:
         w[i]=w[i].replace('\n','')
@@ -7,9 +7,16 @@ for i in range(len(w)):
 import random
 
 def words():
+    '''
+    :return: случайное слово из файла со списком слов для игры.
+    '''
     return random.choice(w)
 
 def max_record(a):
+    '''
+    Если текущий рекорд игрока больше общего, то функция перезапишет рекорд в файле на новый.
+    :param a: количество побед игрока в текущей игре.
+    '''
     rec = open('record.txt')
     general_record = rec.read()
     rec.close()
